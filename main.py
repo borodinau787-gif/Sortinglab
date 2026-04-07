b19c20f37d4e203b594659f4c276c48d2666c37f
borodinau787-gif <borodinau787@gmail.com>
2026-04-07 17:17:18 +0300

    Added Bubble Sort

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import random
 
-def binary_search(arr, val, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] < val:
-            start = mid + 1
-        elif arr[mid] > val:
-            end = mid - 1
-        else:
-            return mid
-    return start
-
-def binary_insertion_sort(arr):
-    for i in range(1, len(arr)):
-        val = arr[i]
-        j = binary_search(arr, val, 0, i - 1)
-        arr = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(20)]
     print("Original:", *data)
-    print("Binary Sort:", *binary_insertion_sort(data.copy()))
+    print("Bubble Sort:", *bubble_sort(data.copy()))
